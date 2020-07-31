fd74155f980960ea73e7999570c9f7dd220a774b
Oğuzhan Çevik <cevik.ogsan@gmail.com>
2020-07-31 18:43:48 +0300

    Naming / Define public and private variables / single and multiple comments

---
+++ 3-naming/main.go
@@ -0,0 +1,20 @@
+package main
+
+// single line comment
+
+func main()  {
+	// ...
+}
+
+// this is private variable because first letter is lowercase
+var name string = "golang"
+
+var Version string = "1.0.2"
+
+/*
+
+multiple
+comment
+lines
+
+*/
