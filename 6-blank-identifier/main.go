34535dd9f2ee6560f1ce74b030c9518c0f999407
Oğuzhan Çevik <cevik.ogsan@gmail.com>
2020-08-09 14:42:39 +0300

    blank identifier (_)

---
+++ 6-blank-identifier/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	// Blank identifier (_)
+	
+	/*
+	
+	_, veri := method();
+	fmt.Println(veri);
+	
+	*/
+
+	// var _ string = "string value";
+
+	var _, x, _ = 15, 9, 6;
+	fmt.Println(x);
+
+}
